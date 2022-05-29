ef61ab8140c4b0999651b7f3aae1c69e5a922660
Mateus Machado <mateusmmachado2@gmail.com>
2022-05-29 14:56:09 -0300

    Add project files.

---
+++ RememberMeBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using DSharpPlus;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+
+var client = new DiscordClient(new DiscordConfiguration
+{
+    Token = "",
+    TokenType = TokenType.Bot
+});
+
+await host.RunAsync();
