ab4fb692c9f945e44d389a1c12a7277f6f12a3c1
Mateus Machado <mateusmmachado2@gmail.com>
2022-07-24 17:30:12 -0300

    feat(worker): added base worker logic

---
+++ RememberMeBot.Worker/Program.cs
@@ -0,0 +1,41 @@
+﻿using RememberMeBot.Worker;
+
+Console.WriteLine("Worker onliners!");
+
+List<DateTime> alarms = new List<DateTime>();
+
+while (true)
+{
+    var alarm = ReceiveAlarm.Receive();
+
+    if (!string.IsNullOrEmpty(alarm))
+    {
+        AddAlarm(alarm);
+    }
+
+    if (alarms.Any())
+    {
+        CheckAlarms(alarms);
+    }
+
+}
+
+void AddAlarm(string alarm)
+{
+    var alarmTime = DateTime.Parse(alarm);
+
+    var alarmDateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, alarmTime.Hour, alarmTime.Minute, alarmTime.Second);
+
+    alarms.Add(alarmDateTime);
+}
+
+void CheckAlarms(List<DateTime> alarms)
+{
+    foreach (var alarm in alarms)
+    {
+        if (alarm == DateTime.Now)
+        {
+            //trigger
+        }
+    }
+}
---
+++ RememberMeBot.Worker/ReceiveAlarm.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RememberMeBot.Worker
+{
+    public static class ReceiveAlarm
+    {
+        public static string Receive()
+        {
+            try
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    var message = string.Empty;
+
+                    channel.QueueDeclare(queue: "alarms",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        message = Encoding.UTF8.GetString(body);
+                    };
+                    channel.BasicConsume(queue: "alarms",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    return message;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return string.Empty;
+            }            
+        }
+    }
+}
